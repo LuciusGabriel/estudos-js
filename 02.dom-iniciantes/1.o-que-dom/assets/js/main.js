82522ea0017190debb0368a6d351042faced705e
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-11-17 22:07:25 -0300

    Estrutura Pastas

---
+++ 02.dom-iniciantes/1.o-que-dom/assets/js/main.js
@@ -0,0 +1,18 @@
+// Retorne o URL da página atual utilizando o objeto window
+console.log(window.location.href)
+
+
+//Selecione o primeiro elemento da página que possua a classe ativo
+let primeiroElemento = document.querySelector('.ativo')
+console.log(primeiroElemento)
+
+
+//Retorne a linguagem do navegador
+
+console.log(navigator.language)
+
+
+
+//Retorne a largura da janela
+
+console.log(window.innerWidth)
