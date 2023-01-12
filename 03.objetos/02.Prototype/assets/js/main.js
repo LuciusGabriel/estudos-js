82e5a2ce1b7660ecad599e747fdbe15a965c09a2
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-01-11 23:28:13 -0300

    Prototype

---
+++ 03.objetos/02.Prototype/assets/js/main.js
@@ -0,0 +1,22 @@
+// Cria uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método do protótipo que retorne
+// o nome completo da pessoa
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelectorAll('li')
+
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+// Qual o construtor do dado abaixo
+li.hidden.constructor.name
