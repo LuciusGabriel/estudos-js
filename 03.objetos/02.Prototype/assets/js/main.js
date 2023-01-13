c5ab7fd8b72c4e5c8d24bf2df2f78b471b617164
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-01-12 21:20:38 -0300

    Prototype Continue

--- 03.objetos/02.Prototype/assets/js/main.js
+++ 03.objetos/02.Prototype/assets/js/main.js
@@ -3,9 +3,34 @@
 // Crie um método do protótipo que retorne
 // o nome completo da pessoa
 
+function Pessoas(nome, sobrenome, idade){
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.idade = idade
+}
+
+Pessoas.prototype.nomeCompleto = function(){
+    // this.nomeCompleto = this.nome + this.sobrenome
+    // return this.nomeCompleto --> COMO EU FIZ
+    // Melhor Prática ↓↓↓↓
+    return `${this.nome} ${this.sobrenome}`
+}
+
+const lucius = new Pessoas('Lucius', 'Silva', 30)
+
+
 // Liste os métodos acessados por
 // dados criados com NodeList,
 // HTMLCollection, Document
+
+// Forma de acessar... ↓↓↓
+console.log(Object.getOwnPropertyNames(NodeList.prototype))
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype))
+console.log(Object.getOwnPropertyNames(Document.prototype))
+
+
+
+
 
 // Liste os construtores dos dados abaixo
 const li = document.querySelectorAll('li')
