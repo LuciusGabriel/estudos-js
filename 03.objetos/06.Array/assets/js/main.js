6b09a7116922c67c063076c3fad622a8c5bcf56f
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-02-01 22:35:14 -0300

    Inicia Estudos Array

---
+++ 03.objetos/06.Array/assets/js/main.js
@@ -0,0 +1,11 @@
+// const li = document.querySelectorAll('li')
+// console.log(li)
+// console.log(Array.isArray(li))
+// console.log(Array.from(li))
+
+
+// console.log(Array.of(10, 20, 30))
+
+
+
+
