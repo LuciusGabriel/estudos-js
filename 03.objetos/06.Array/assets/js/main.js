abb1d054ac149ec7871c98bb13a628b8ccea4541
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-02-03 10:31:53 -0300

    Novos Exercícios de Array

--- 03.objetos/06.Array/assets/js/main.js
+++ 03.objetos/06.Array/assets/js/main.js
@@ -71,3 +71,80 @@ console.log(carrosOriginal)
 
 carros.pop()
 console.log(carros)
+
+
+
+
+
+
+
+// Exercícios Complementares Gerados pelo ChatGPT
+
+// 1. Crie um array com cinco elementos e exiba cada um no console.
+let novaArray = ['0', '1', '2', '3', '4']
+console.log(novaArray)
+
+for(i=0; i<novaArray.length; i++){
+  console.log(novaArray[i])
+}
+
+// 2. Adicione mais três elementos ao array criado no exercício 1 e exiba o array completo no console.
+let outraArray = novaArray.slice()
+console.log(outraArray)
+
+outraArray.push('5', '6', '7')
+console.log(outraArray)
+
+// 3. Remova o último elemento do array do exercício 2 e exiba o array atualizado no console.
+outraArray.pop()
+console.log(outraArray)
+
+// 4. Adicione um novo elemento ao início do array do exercício 3 e exiba o array atualizado no console.
+outraArray.unshift('-1')
+console.log(outraArray)
+
+for(i=0; i<outraArray.length; i++){
+  console.log(outraArray[i])
+}
+
+// 5. Crie um novo array com três elementos e concatene-o com o array do exercício 4 para criar um novo array. 
+// Exiba o novo array no console.
+let novaArray3 = ['8', '9', '10']
+
+let arrayConcatenada = outraArray.concat(novaArray3)
+console.log(arrayConcatenada)
+
+// 6. Encontre o segundo menor elemento do novo array do exercício 5 e exiba-o no console.
+let arrayDesordenado = [5, 8, 6, 1, 10]
+
+let arrayOrdenado = arrayDesordenado.sort((a, b)=>{
+  return a - b
+})
+
+let segundoMenorValor = arrayOrdenado[1]
+
+console.log(arrayOrdenado)
+console.log(segundoMenorValor)
+
+// 7. Crie uma função que receba um array como argumento e retorne o array inverso. 
+// Teste sua função com o novo array do exercício 5.
+function inverteArray(array){
+  let arrayInvertida = array.slice()
+  arrayInvertida.reverse()
+  console.log(arrayInvertida)
+}
+
+inverteArray(arrayConcatenada)
+
+// 8. Crie uma função que receba dois arrays como argumentos e retorne um array com todos os elementos dos dois arrays. 
+// Teste sua função com o novo array do exercício 5 e outro array de sua escolha.
+
+
+let arrayParaConcatenar = ['100', '200', '300']
+
+function concatenaArrays(array1, array2){
+  let novaArray = array1.concat(array2)
+    console.log(novaArray)
+}
+
+concatenaArrays(arrayConcatenada, arrayParaConcatenar)
